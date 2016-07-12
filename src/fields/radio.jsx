24532d1ba5d0d9cd99937d20783690eef6884f0f
Nicolás López <nicolaslopezj@me.com>
2016-07-12 15:24:18 -0400

    add more features to material-ui select with method

--- src/fields/radio.jsx
+++ src/fields/radio.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import RadioButton from 'material-ui/RadioButton';
-import * as Colors from 'material-ui/styles/colors';
-import {FieldType, registerType} from 'simple-react-form';
-import styles from '../styles';
+import React from 'react'
+import RadioButton from 'material-ui/RadioButton'
+import * as Colors from 'material-ui/styles/colors'
+import {FieldType, registerType} from 'simple-react-form'
+import styles from '../styles'
 
 const propTypes = {
   /**
@@ -12,25 +12,25 @@ const propTypes = {
     label: React.PropTypes.string.isRequired,
     value: React.PropTypes.oneOfType([
       React.PropTypes.string,
-      React.PropTypes.number,
+      React.PropTypes.number
     ]).isRequired,
-    description: React.PropTypes.string,
-  })).isRequired,
-};
+    description: React.PropTypes.string
+  })).isRequired
+}
 
 const defaultProps = {
 
-};
+}
 
 class RadioComponent extends FieldType {
 
-  renderItems() {
+  renderItems () {
     return this.props.options.map((item) => {
       return (
         <div key={item.value}>
           <RadioButton
           label={item.label}
-          checked={item.value == this.props.value}
+          checked={item.value === this.props.value}
           onCheck={() => this.props.onChange(item.value)}
           disabled={this.props.disabled}
           style={{ marginBotton: 16, marginTop: 16 }}
@@ -38,14 +38,14 @@ class RadioComponent extends FieldType {
           <div
           style={{ marginLeft: 40, color: Colors.grey500, cursor: 'pointer' }}
           onClick={() => this.props.onChange(item.value)}>
-            {(item.description || '').split('\n').map((text, index) => <div key={index}>{text}</div>)}
+            {(item.description || '').split('\n').map((text, index) => <div key={index}>{text}</div>)}
           </div>
         </div>
-      );
-    });
+      )
+    })
   }
 
-  render() {
+  render () {
     return (
       <div style={styles.fieldContainer}>
         <div style={styles.mirrorLabel}>
@@ -54,16 +54,14 @@ class RadioComponent extends FieldType {
         {this.renderItems()}
         <div style={styles.errorMessage}>{this.props.errorMessage}</div>
       </div>
-    );
+    )
   }
 }
 
-RadioComponent.propTypes = propTypes;
-RadioComponent.defaultProps = defaultProps;
+RadioComponent.propTypes = propTypes
+RadioComponent.defaultProps = defaultProps
 
 registerType({
   type: 'radio',
-  component: RadioComponent,
-  allowedTypes: [String, Number],
-  description: 'Simple radio field.',
-});
+  component: RadioComponent
+})
--- src/fields/select-with-method.jsx
+++ src/fields/select-with-method.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import AutoComplete from 'material-ui/AutoComplete';
-import MenuItem from 'material-ui/MenuItem';
-import CircularProgress from 'material-ui/CircularProgress';
-import * as Colors from 'material-ui/styles/colors';
-import {FieldType, registerType} from 'simple-react-form';
-import styles from '../styles';
+import React from 'react'
+import AutoComplete from 'material-ui/AutoComplete'
+import MenuItem from 'material-ui/MenuItem'
+import CircularProgress from 'material-ui/CircularProgress'
+import _ from 'underscore'
+import {FieldType, registerType} from 'simple-react-form'
+import Chip from 'material-ui/Chip'
+import * as Colors from 'material-ui/styles/colors'
+import Avatar from 'material-ui/Avatar'
+import FontIcon from 'material-ui/FontIcon'
 
 const propTypes = {
   /**
@@ -13,7 +16,7 @@ const propTypes = {
   multi: React.PropTypes.bool,
   /**
    * Meteor method that recieves the search string and returns an array of items
-   * with "label" and "value" attributes.
+   * with 'label' and 'value' attributes.
    */
   methodName: React.PropTypes.string.isRequired,
   /**
@@ -41,242 +44,268 @@ const propTypes = {
   /**
    * A function that returns if a value can be created
    */
-  canCreate: React.PropTypes.func,
-};
+  canCreate: React.PropTypes.func
+}
 
 const defaultProps = {
   multi: false,
   waitTime: 400,
-  createLabel: (search) => `Create "${search}"`,
-  canCreate: () => true,
-};
+  createLabel: (search) => `Create '${search}'`,
+  canCreate: () => true
+}
 
 class SelectWithMethodComponent extends FieldType {
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       dataSource: [],
       selected: null,
       items: [],
-      knownLabels: [],
+      knownItems: [],
       response: [],
       isFetchingData: false,
       isFetchingLabel: false,
       hasTitleFor: null,
-      searchText: '',
-    };
+      searchText: ''
+    }
 
-    this.debouncedSearch = _.debounce(this.search.bind(this), this.props.waitTime);
+    this.debouncedSearch = _.debounce(this.search.bind(this), this.props.waitTime)
   }
 
-  isLoading() {
-    return this.state.isFetchingData || this.state.isFetchingLabel;
+  isLoading () {
+    return this.state.isFetchingData || this.state.isFetchingLabel
   }
 
-  componentDidMount() {
-    this.updateLabel(this.props.value);
+  componentDidMount () {
+    this.updateLabel(this.props.value)
   }
 
-  componentWillReceiveProps(nextProps) {
-    //Console.log('will recieve props', nextProps);
+  componentWillReceiveProps (nextProps) {
+    // console.log('will recieve props', nextProps)
     if (this.props.value !== nextProps.value && nextProps.value) {
-      this.updateLabel(nextProps.value);
+      this.updateLabel(nextProps.value)
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate (prevProps, prevState) {
     if (this.state.searchText !== this.refs.input.state.searchText) {
-      this.refs.input.setState({ searchText: this.state.searchText });
+      this.refs.input.setState({ searchText: this.state.searchText })
     }
   }
 
-  updatedSelectedItems(values) {
-    var missingLabels = [];
-    var selectedItems = [];
-    var knownLabels = this.state.knownLabels;
-    var valueArray = _.isArray(values) ? values : [values];
+  updatedSelectedItems (values) {
+    var missingLabels = []
+    var knownItems = this.state.knownItems
+    var valueArray = _.isArray(values) ? values : [values]
 
-    if (!values) return;
+    if (!values) return
 
     valueArray.map((value) => {
-      if (!this.state.knownLabels[value]) {
-        missingLabels.push(value);
+      if (!this.state.knownItems[value]) {
+        missingLabels.push(value)
       }
-    });
+    })
 
     if (missingLabels.length > 0) {
-      var labelMethodName = this.props.labelMethodName;
-      var connection = this.props.connection || Meteor;
-      var labelsMethod = this.props.multi ? missingLabels : missingLabels[0];
-      this.setState({isFetchingLabel: true});
+      var labelMethodName = this.props.labelMethodName
+      var connection = this.props.connection || Meteor
+      var labelsMethod = this.props.multi ? missingLabels : missingLabels[0]
+      this.setState({isFetchingLabel: true})
       connection.call(labelMethodName, labelsMethod, (error, response) => {
-        this.setState({isFetchingLabel: false});
+        this.setState({isFetchingLabel: false})
         if (error) {
-          console.log(`[select-with-method] Recieved error from "${labelMethodName}"`, error);
-        } else {
+          console.log(`[select-with-method] Recieved error from '${labelMethodName}'`, error)
+        } else {
           if (this.props.multi) {
             missingLabels.map((value, index) => {
-              knownLabels[value] = response[index];
-            });
+              if (_.isString(response[index])) {
+                knownItems[value] = {label: response[index]}
+              } else {
+                knownItems[value] = response[index]
+              }
+            })
           } else {
-            knownLabels[labelsMethod] = response;
-
-            //Console.log('setting to response', response);
-            this.setState({ searchText: response });
+            if (_.isString(response)) {
+              knownItems[labelsMethod] = {label: response}
+            } else {
+              knownItems[labelsMethod] = response
+            }
+            // console.log('setting to response', response)
+            this.setState({ searchText: knownItems[labelsMethod].label })
           }
 
-          this.setState({ knownLabels });
+          this.setState({ knownItems })
         }
-      });
+      })
     } else {
       if (!this.props.multi) {
-        //Console.log('setting to known label', knownLabels[values]);
-        this.setState({ searchText: knownLabels[values] });
+        // console.log('setting to known label', knownItems[values])
+        this.setState({ searchText: knownItems[values] })
       }
     }
   }
 
-  updateLabel(value) {
+  updateLabel (value) {
     if (!this.props.multi && !value) {
-      //Console.log('clean on update');
-      this.setState({ searchText: '' });
-      return;
+      // console.log('clean on update')
+      this.setState({ searchText: '' })
+      return
     }
 
-    this.updatedSelectedItems(value);
+    this.updatedSelectedItems(value)
   }
 
-  search(text) {
-    //Console.log('searching with text', text);
-    this.setState({selected: null, isFetchingData: true});
+  search (text) {
+    // console.log('searching with text', text)
+    this.setState({selected: null, isFetchingData: true})
 
     if (!this.props.multi) {
-      this.props.onChange(null);
+      this.props.onChange(null)
     }
 
-    var methodName = this.props.methodName;
-    var connection = this.props.connection || Meteor;
+    var methodName = this.props.methodName
+    var connection = this.props.connection || Meteor
     connection.call(methodName, text, (error, response) => {
-      this.setState({isFetchingData: false});
+      this.setState({isFetchingData: false})
       if (error) {
-        console.log(`[select-with-method] Recieved error from "${methodName}"`, error);
+        console.log(`[select-with-method] Recieved error from '${methodName}'`, error)
       } else {
-        response = response || [];
-        this.setState({ response });
+        response = response || []
+        this.setState({ response })
         var dataSource = response.map((item) => {
           return {
             text: item.value,
-            value: <MenuItem primaryText={item.label} />,
-          };
-        });
+            value: <MenuItem primaryText={item.label} />
+          }
+        })
         if (_.isFunction(this.props.create) && text && this.props.canCreate(text)) {
           dataSource.push({
             text: text,
-            value: <MenuItem primaryText={this.props.createLabel(text)} />,
+            value: <MenuItem primaryText={this.props.createLabel(text)} />
           })
         }
-        this.setState({ dataSource });
+        this.setState({ dataSource })
       }
-    });
+    })
   }
 
-  onUpdateText(text) {
-    this.setState({searchText: text, isFetchingData: true});
-    this.debouncedSearch(text);
+  onUpdateText (text) {
+    this.setState({searchText: text, isFetchingData: true})
+    this.debouncedSearch(text)
   }
 
-  createItem(item) {
+  createItem (item) {
     this.props.create(item.text, (value) => {
       if (this.props.multi) {
         setTimeout(() => {
-          this.setState({ searchText: '' });
-        }, 101);
-        if (_.contains(this.props.value || [], value)) {
-          return;
+          this.setState({ searchText: '' })
+        }, 101)
+        if (_.contains(this.props.value || [], value)) {
+          return
         }
-        this.props.onChange(_.union(this.props.value || [], [value]));
+        this.props.onChange(_.union(this.props.value || [], [value]))
       } else {
-        this.props.onChange(value);
+        this.props.onChange(value)
       }
-    });
+    })
   }
 
-  onItemSelected(item, index) {
-    if (index == this.state.response.length && _.isFunction(this.props.create)) {
-      return this.createItem(item);
+  onItemSelected (item, index) {
+    if (index === this.state.response.length && _.isFunction(this.props.create)) {
+      return this.createItem(item)
     }
-    var selected = this.state.response[index];
+    var selected = this.state.response[index]
     if (this.props.multi) {
-      //Console.log('clean on item selected');
+      // console.log('clean on item selected')
       setTimeout(() => {
-        this.setState({ searchText: '' });
-      }, 101);
-      if (_.contains(this.props.value || [], selected.value)) return;
-      this.props.onChange(_.union(this.props.value || [], [selected.value]));
+        this.setState({ searchText: '' })
+      }, 101)
+      if (_.contains(this.props.value || [], selected.value)) return
+      this.props.onChange(_.union(this.props.value || [], [selected.value]))
     } else {
-      this.props.onChange(selected ? selected.value : null);
+      this.props.onChange(selected ? selected.value : null)
       setTimeout(() => {
-        this.setState({ searchText: selected.label });
-      }, 101);
+        this.setState({ searchText: selected.label })
+      }, 101)
     }
 
     if (selected) {
-      this.state.knownLabels[selected.value] = selected.label;
-      this.setState({ knownLabels: this.state.knownLabels });
+      this.state.knownItems[selected.value] = selected
+      this.setState({ knownItems: this.state.knownItems })
     }
   }
 
-  removeItem(value) {
-    this.props.onChange(_.without(this.props.value || [], value));
+  removeItem (value) {
+    this.props.onChange(_.without(this.props.value || [], value))
   }
 
-  onFocus() {
+  onFocus () {
     if (!this.props.multi && !this.props.value) {
-      this.search('');
+      this.search('')
     }
   }
 
-  onBlur() {
-    this.setState({ open: false });
+  onBlur () {
+    this.setState({ open: false })
     if (!this.props.value) {
-      this.setState({ searchText: '' });
+      this.setState({ searchText: '' })
     }
 
     if (this.state.searchText !== this.refs.input.state.searchText) {
-      //console.log('did blur, not equal');
-      this.refs.input.setState({ searchText: this.state.searchText });
+      // console.log('did blur, not equal')
+      this.refs.input.setState({ searchText: this.state.searchText })
     }
   }
 
-  renderItems() {
-    return (_.isArray(this.props.value) ? this.props.value : []).map((value, index) => {
-      var label = this.state.knownLabels[value] || 'Loading...';
+  renderItems () {
+    return (_.isArray(this.props.value) ? this.props.value : []).map((value, index) => {
+      const item = this.state.knownItems[value] || 'Loading...'
+      const label = item.label
+      const image = item.image
+      const initials = item.initials || undefined
+      const color = item.color
+      const textColor = color ? Colors.white : Colors.grey900
+      const icon = item.icon ? <FontIcon className='material-icons'>{item.icon}</FontIcon> : null
+      let avatar = null
+      if (initials || icon || image) {
+        avatar = (
+          <Avatar src={image} size={32} icon={icon} color={Colors.blue200} backgroundColor={Colors.blue600}>
+            {initials}
+          </Avatar>
+        )
+      }
       return (
-        <div onClick={() => this.removeItem(value)} key={value} style={styles.tag}>
+        <Chip
+        onRequestDelete={() => this.removeItem(value)}
+        key={value}
+        labelColor={textColor}
+        style={{marginBottom: 3}}
+        backgroundColor={color}>
+          {avatar}
           {label}
-        </div>
-      );
-    });
+        </Chip>
+      )
+    })
   }
 
-  renderLoading() {
-    if (!this.isLoading()) return;
+  renderLoading () {
+    if (!this.isLoading()) return
 
     return (
       <CircularProgress
       style={{float: 'right', marginTop: -55, marginRight: -6}}
       size={0.4}/>
-    );
+    )
   }
 
-  render() {
+  render () {
     return (
       <div>
         <AutoComplete
         ref='input'
-        fullWidth={true}
-        searchText=""
+        fullWidth
+        searchText=''
         dataSource={this.state.dataSource}
         filter={AutoComplete.noFilter}
         onUpdateInput={this.onUpdateText.bind(this)}
@@ -287,7 +316,7 @@ class SelectWithMethodComponent extends FieldType {
         onFocus={this.onFocus.bind(this)}
         onBlur={this.onBlur.bind(this)}
         open={this.state.open}
-        openOnFocus={true}
+        openOnFocus
         disabled={this.props.disabled}
         menuCloseDelay={100}
         {...this.passProps} />
@@ -296,15 +325,14 @@ class SelectWithMethodComponent extends FieldType {
           {this.renderItems()}
         </div>
       </div>
-    );
+    )
   }
 }
 
-SelectWithMethodComponent.propTypes = propTypes;
-SelectWithMethodComponent.defaultProps = defaultProps;
+SelectWithMethodComponent.propTypes = propTypes
+SelectWithMethodComponent.defaultProps = defaultProps
 
 registerType({
   type: 'select-with-method',
-  component: SelectWithMethodComponent,
-  description: 'A select input that connects to a Meteor Method to fetch data.',
-});
+  component: SelectWithMethodComponent
+})
