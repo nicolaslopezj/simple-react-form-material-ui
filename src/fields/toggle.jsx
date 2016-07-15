0220853ff1c9e78aec0e918e7356e8e20e51941f
Nicolás López <nicolaslopezj@me.com>
2016-07-15 11:51:20 -0400

    remove full width from switch

--- src/fields/toggle.jsx
+++ src/fields/toggle.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import Toggle from 'material-ui/Toggle';
-import {FieldType, registerType} from 'simple-react-form';
-import styles from '../styles';
+import React from 'react'
+import Toggle from 'material-ui/Toggle'
+import {FieldType, registerType} from 'simple-react-form'
+import styles from '../styles'
 
 const propTypes = {
 
-};
+}
 
 const defaultProps = {
 
-};
+}
 
 class ToggleComponent extends FieldType {
 
-  render() {
+  render () {
     return (
       <div>
         <Toggle
           label={this.props.label}
-          fullWidth={true}
           defaultToggled={!!this.props.value}
           disabled={this.props.disabled}
           onToggle={() => this.props.onChange(!this.props.value)}
           {...this.passProps}/>
         <div style={styles.errorMessage}>{this.props.errorMessage}</div>
       </div>
-    );
+    )
   }
 
 }
 
-ToggleComponent.propTypes = propTypes;
-ToggleComponent.defaultProps = defaultProps;
+ToggleComponent.propTypes = propTypes
+ToggleComponent.defaultProps = defaultProps
 
 registerType({
   type: 'toggle',
-  component: ToggleComponent,
-  allowedTypes: [Boolean],
-  description: 'Material UI toggle.',
-});
+  component: ToggleComponent
+})
