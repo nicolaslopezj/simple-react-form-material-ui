a897df738f849aa7d6f5a720bac0c45a86296d51
Nicolás López <nicolaslopezj@me.com>
2016-07-28 22:07:05 -0400

    Use context instead of cloning elements

--- src/fields/array.jsx
+++ src/fields/array.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Paper from 'material-ui/Paper';
-import IconButton from 'material-ui/IconButton';
-import RaisedButton from 'material-ui/RaisedButton';
-import { Form, ArrayComponent } from 'simple-react-form';
+import React from 'react'
+import Paper from 'material-ui/Paper'
+import IconButton from 'material-ui/IconButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import { Form, ArrayComponent } from 'simple-react-form'
 
 const styles = {
   label: {
     color: 'rgba(0,0,0,0.5)',
     marginBottom: 5,
-    fontSize: 12,
-  },
-};
+    fontSize: 12
+  }
+}
 
 const propTypes = {
   ...ArrayComponent.propTypes,
   parentClassName: React.PropTypes.string,
   childrenClassName: React.PropTypes.string,
   useSmallSpace: React.PropTypes.bool,
-  smallRemoveButtonTooltipPosition: React.PropTypes.string,
-};
+  smallRemoveButtonTooltipPosition: React.PropTypes.string
+}
 
 const defaultProps = {
   ...ArrayComponent.defaultProps,
@@ -26,81 +26,82 @@ const defaultProps = {
   parentClassName: '',
   useSmallSpace: false,
   smallRemoveButtonTooltipPosition: 'bottom-center'
-};
+}
 
 export default class MaterialArray extends ArrayComponent {
-  renderChildrenItem({ index, component }) {
-    if (this.props.useSmallSpace) return this.renderChildrenSmallItem({ index, component });
+
+  renderChildrenItem ({ index, children }) {
+    if (this.props.useSmallSpace) return this.renderChildrenSmallItem({ index, children })
     return (
       <div className={this.props.childrenClassName} key={`${this.props.fieldName}.${index}`}>
         <Paper style={{ marginTop: 20, marginBottom: 20, padding: 20 }}>
-          {component}
+          {this.renderChildrenItemWithContext({index, children})}
           <div style={{ marginTop: 10, textAlign: 'right' }}>
             {this.renderRemoveButton(index)}
           </div>
         </Paper>
       </div>
-    );
+    )
   }
 
-  renderChildrenSmallItem({ index, component }) {
+  renderChildrenSmallItem ({ index, children }) {
     return (
       <div className={this.props.childrenClassName} key={`${this.props.fieldName}.${index}`} style={{ marginTop: 10, marginBottom: 10, display: 'flex' }}>
         <div style={{ flexBasis: '90%', maxWidth: '90%' }}>
-          {component}
+          {this.renderChildrenItemWithContext({index, children})}
         </div>
         <div style={{ flexBasis: '10%', maxWidth: '10%', marginTop: 20, textAlign: 'right' }}>
           {this.renderSmallRemoveButton(index)}
         </div>
       </div>
-    );
+    )
   }
 
-  renderRemoveButton(index) {
-    if (this.props.disabled) return;
-    return <RaisedButton label={this.props.removeLabel} onTouchTap={() => this.removeItem(index)}/>;
+  renderRemoveButton (index) {
+    if (this.props.disabled) return
+    return <RaisedButton label={this.props.removeLabel} onTouchTap={() => this.removeItem(index)}/>
   }
 
-  renderSmallRemoveButton(index) {
-    if (this.props.disabled) return;
+  renderSmallRemoveButton (index) {
+    if (this.props.disabled) return
     return (
       <IconButton
-        iconClassName="material-icons"
+        iconClassName='material-icons'
         onTouchTap={() => this.removeItem(index)}
         tooltip={this.props.removeLabel}
         tooltipPosition={this.props.smallRemoveButtonTooltipPosition}
         >
         clear
       </IconButton>
-    );
+    )
   }
 
-  renderAddButton() {
-    if (!this.props.showAddButton) return;
-    if (this.props.disabled) return;
-    if (this.props.useSmallSpace) return this.renderSmallAddButton();
-    return <RaisedButton label={this.props.addLabel} onTouchTap={() => this.addItem()}/>;
+  renderAddButton () {
+    if (!this.props.showAddButton) return
+    if (this.props.disabled) return
+    if (this.props.useSmallSpace) return this.renderSmallAddButton()
+    return <RaisedButton label={this.props.addLabel} onTouchTap={() => this.addItem()}/>
   }
 
-  renderSmallAddButton() {
+  renderSmallAddButton () {
     return (
       <div style={{ textAlign: 'right' }}>
         <IconButton
-          iconClassName="material-icons"
+          iconClassName='material-icons'
           onTouchTap={() => this.addItem()}
           tooltip={this.props.addLabel}
           >
           add
         </IconButton>
       </div>
-    );
+    )
   }
 
-  render() {
+  render () {
     return (
       <div style={{ marginTop: 20 }}>
         <div style={styles.label}>{this.getLabel()}</div>
-        <div style={{ color: 'red' }}>{this.props.errorMessage}</div>
+        <div style={{ color: 'red' }}>{this.getErrorMessage()}</div>
         <div className={this.props.parentClassName}>
           {this.renderChildren()}
         </div>
@@ -108,13 +109,10 @@ export default class MaterialArray extends ArrayComponent {
           {this.renderAddButton()}
         </div>
       </div>
-    );
+    )
   }
 }
 
-MaterialArray.propTypes = propTypes;
-MaterialArray.defaultProps = defaultProps;
-
-Form.defaultProps.arrayComponent = MaterialArray;
-
-//ArrayComponent = MaterialArray;
+MaterialArray.propTypes = propTypes
+MaterialArray.defaultProps = defaultProps
+Form.defaultProps.arrayComponent = MaterialArray
--- src/fields/object.jsx
+++ src/fields/object.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import Paper from 'material-ui/Paper';
-import { Form, ObjectComponent } from 'simple-react-form';
+import React from 'react'
+import Paper from 'material-ui/Paper'
+import { Form, ObjectComponent } from 'simple-react-form'
 
 const styles = {
   label: {
     color: 'rgba(0,0,0,0.5)',
     marginBottom: 5,
-    fontSize: 12,
-  },
-};
-
-export default class MaterialObject extends ObjectComponent {
-  render() {
-    return (
-      <Paper style={{ marginTop: 20, marginBottom: 20, padding: 20 }}>
-        <div style={styles.label}>{this.getLabel()}</div>
-        <div style={{ color: 'red' }}>{this.props.errorMessage}</div>
-        {this.renderChildren(this.props.children)}
-      </Paper>
-    );
+    fontSize: 12
   }
 }
 
-//MRF.Object = MaterialObject;
-Form.defaultProps.objectComponent = MaterialObject;
+export default class MaterialObject extends ObjectComponent {
 
-//ObjectComponent = MaterialObject;
+  render () {
+    return (
+      <Paper style={{ marginTop: 20, marginBottom: 20, padding: 20 }}>
+        <div style={styles.label}>{this.getLabel()}</div>
+        <div style={{ color: 'red' }}>{this.getErrorMessage()}</div>
+        {this.props.children}
+      </Paper>
+    )
+  }
+
+}
+
+Form.defaultProps.objectComponent = MaterialObject
